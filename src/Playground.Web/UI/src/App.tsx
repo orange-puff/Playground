39858acffe9123690157e64ca20792dee841156e
John Mancini <johnmancini30@gmail.com>
2021-07-04 16:15:42 -0400

    latest

--- src/Playground.Web/UI/src/App.tsx
+++ src/Playground.Web/UI/src/App.tsx
@@ -6,6 +6,7 @@ import {
 import Home from './components/home/Home';
 import Layout from './components/layout/Layout';
 import Projects from './components/projects/Projects';
+import JsonProject from './components/projects/json_project/JsonProject'
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/projects" component={Projects} />
+                <Route exact path="/projects/json_project" component={JsonProject} />
             </Switch>
         </Layout>
     );
--- src/Playground.Web/UI/src/components/projects/json_project/JsonProject.tsx
+++ src/Playground.Web/UI/src/components/projects/json_project/JsonProject.tsx
@@ -1,10 +1,37 @@
 ﻿import React from 'react'
+import Button from '@material-ui/core/Button';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+import ClearAllIcon from '@material-ui/icons/ClearAll';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    button: {
+        margin: theme.spacing(1),
+    },
+}));
 
 const JsonProject = () => {
+    const styles = useStyles();
+
     return (
-        <p>
-            My Project
-            </p>
+        <div>
+            <Button
+                variant="contained"
+                color="secondary"
+                className={styles.button}
+                startIcon={<ClearAllIcon />}
+            >
+                Clear
+            </Button>
+            <Button
+                variant="contained"
+                className={styles.button}
+                startIcon={<DoubleArrowIcon/>}
+            >
+                Validate
+            </Button>
+        </div>
     )
 }
 

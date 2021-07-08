0b8b4b97231008ce2a2b1f793236bbc86d33dc90
John Mancini <johnmancini30@gmail.com>
2021-07-07 21:48:50 -0400

    added nice comment

--- src/JsonProject/JsonHelper.cs
+++ src/JsonProject/JsonHelper.cs
@@ -3,6 +3,52 @@ using System.Collections.Generic;
 
 namespace JsonProject
 {
+    /// <summary>
+    /// <list type="bullet">
+    /// <item>
+    /// <description>Data is in name/value pairs</description>
+    /// </item>
+    /// <item>
+    /// <description>Data is separated by commas</description>
+    /// </item>
+    /// <item>
+    /// <description>Objects are encapsulated within the opening and closing curly brackets</description>
+    /// </item>
+    /// <item>
+    /// <description>An empty object can be represented by {}</description>
+    /// </item>
+    /// <item>
+    /// <description>Arrays are encapsulated within opening and closing square brackets</description>
+    /// </item>
+    /// <item>
+    /// <description>An empty array can be represented by []</description>
+    /// </item>
+    /// <item>
+    /// <description>A member is represented by a key-value pair, contained in double quotes</description>
+    /// </item>
+    /// <item>
+    /// <description>Each member should have a unique key within an object structure</description>
+    /// </item>
+    /// <item>
+    /// <description>The value of a member must be contained in double quotes, if it's a string</description>
+    /// </item>
+    /// <item>
+    /// <description>Boolean values are represented using the true or false literals in lower case</description>
+    /// </item>
+    /// <item>
+    /// <description>Number values are represented using double-precision floating-point format and shouldn't have leading zeroes</description>
+    /// </item>
+    /// <item>
+    /// <description>"Offensive" characters in a string need to be escaped using the backslash character Null values are represented by the null literal in lower case</description>
+    /// </item>
+    /// <item>
+    /// <description>Dates, and similar object types, aren't adequately supported and should be converted to strings</description>
+    /// </item>
+    /// <item>
+    /// <description>Each member of an object or array value must be followed by a comma, except for the last one</description>
+    /// </item>
+    /// </list>
+    /// </summary>
     public static class JsonHelper
     {
         public static bool TryFormat(out string json, out string error)

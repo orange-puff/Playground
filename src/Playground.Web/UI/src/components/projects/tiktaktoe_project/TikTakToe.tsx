a9a386be675ec0c4157e6975dcaa13519beb02c4
John Mancini <johnmancini30@gmail.com>
2021-07-13 09:32:31 -0400

    game is fully functionaly now

--- src/Playground.Web/UI/src/components/projects/tiktaktoe_project/TikTakToe.tsx
+++ src/Playground.Web/UI/src/components/projects/tiktaktoe_project/TikTakToe.tsx
@@ -7,20 +7,74 @@ interface ITikTakToeProps {
     n: number
 }
 
+interface ITikTakToeBoardState {
+    n: number,
+    rowSum: number[],
+    colSum: number[],
+    mainDiagSum: number,
+    offDiagSum: number,
+    gameOver: boolean,
+    winningPiece: string
+}
+
 const TikTakToe = (props: React.PropsWithChildren<ITikTakToeProps>) => {
     const { n } = props;
     const [board, setBoard] = useState(Array(n).fill(Array(n).fill('')));
     const [piece, setPiece] = useState('X');
+    const [boardState, setBoardState] = useState<ITikTakToeBoardState>(emptyBoardState());
+
+    function emptyBoardState() {
+        return {
+            n: n,
+            rowSum: Array(n).fill(0),
+            colSum: Array(n).fill(0),
+            mainDiagSum: 0,
+            offDiagSum: 0,
+            gameOver: false,
+            winningPiece: ''
+        } as ITikTakToeBoardState;
+    }
+
+    function cloneBoardState() {
+        return {
+            n: boardState.n,
+            rowSum: Object.assign([], boardState.rowSum),
+            colSum: Object.assign([], boardState.colSum),
+            mainDiagSum: boardState.mainDiagSum,
+            offDiagSum: boardState.offDiagSum,
+            gameOver: boardState.gameOver,
+            winningPiece: boardState.winningPiece
+        } as ITikTakToeBoardState;
+    }
+
+    function updateBoardState(i: number, j: number, piece: string) {
+        const tmp = cloneBoardState();
+        tmp.rowSum[i] += 1;
+        tmp.colSum[j] += 1;
+        if (i === j) {
+            tmp.mainDiagSum += 1;
+        }
+        if (Math.abs(tmp.n - 1 - j) == i) {
+            tmp.offDiagSum += 1;
+        }
+        if (tmp.rowSum[i] === tmp.n || tmp.colSum[j] === tmp.n || tmp.mainDiagSum === tmp.n || tmp.offDiagSum === tmp.n) {
+            tmp.gameOver = true;
+            tmp.winningPiece = piece;
+        }
+
+        setBoardState(tmp);
+    }
 
     function handleClick(i: number, j: number) {
-        if (board[i][j] != '') {
+        if (board[i][j] != '' || boardState.gameOver) {
             return;
         }
 
         const tmp: any = [];
         board.forEach(row => tmp.push(Object.assign([], row)));
         tmp[i][j] = piece;
-        setPiece(piece == 'X' ? 'O' : 'X');
+        updateBoardState(i, j, piece);
+        setPiece(piece === 'X' ? 'O' : 'X');
         setBoard(tmp);
     }
 
@@ -40,10 +94,14 @@ const TikTakToe = (props: React.PropsWithChildren<ITikTakToeProps>) => {
                 variant="contained"
                 color="secondary"
                 startIcon={<ClearAllIcon />}
-                onClick={() => { setPiece('X'); setBoard(Array(n).fill(Array(n).fill(''))); }}
+                onClick={() => { setPiece('X'); setBoard(Array(n).fill(Array(n).fill(''))); setBoardState(emptyBoardState()) }}
             >
                 Clear
                 </Button>
+            {boardState.gameOver
+                ? <p>Game Over! {boardState.winningPiece} Wins!</p>
+                : <p> Game On!</p>
+            }
         </div>
     );
 }

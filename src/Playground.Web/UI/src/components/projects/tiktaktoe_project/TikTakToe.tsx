7b68290e84d32ae197528a4ce98102fced62e5d3
John Mancini <johnmancini30@gmail.com>
2021-07-16 00:47:47 -0400

    useless refactor

--- src/Playground.Web/UI/src/components/projects/tiktaktoe_project/TikTakToe.tsx
+++ src/Playground.Web/UI/src/components/projects/tiktaktoe_project/TikTakToe.tsx
@@ -1,6 +1,6 @@
-﻿import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
+﻿import Button from '@material-ui/core/Button';
 import ClearAllIcon from '@material-ui/icons/ClearAll';
+import React, { useState } from 'react';
 import './TikTakToe.css';
 
 interface ITikTakToeProps {
@@ -14,21 +14,112 @@ interface ITikTakToeBoardState {
     mainDiagSum: number,
     offDiagSum: number,
     gameState: GameState,
-    winningPiece: string
+    piece: string,
+    board: string[][]
 }
 
 enum GameState {
-    noWins = 0,
+    on = 0,
     xWins = 1,
     oWins = 2,
     tie = 3
 }
 
+function numMovesLeft(board: string[][], n: number, offset: number) {
+    let remainingEmpty = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === '') {
+                remainingEmpty++;
+            }
+        }
+    }
+
+    return Math.floor(remainingEmpty / 2) + offset;
+}
+
+function winPossible(state: ITikTakToeBoardState, inc: number, movesLeft: number): boolean {
+    //console.log(state.piece);
+    /* find if a row win is possible */
+    for (let i = 0; i < state.n; i++) {
+        let totEmpty = 0;
+        for (let j = 0; j < state.n; j++) {
+            totEmpty += state.board[i][j] === '' ? inc : 0;
+        }
+        if (Math.abs(totEmpty) < movesLeft && Math.abs(state.rowSum[i] + totEmpty) === state.n) {
+            //console.log(`Row ${i} Empty in Row: ${totEmpty} Row Sum: ${state.rowSum[i]}`);
+            return true;
+        }
+    }
+
+    /* find if a col win is possible */
+    for (let i = 0; i < state.n; i++) {
+        let totEmpty = 0;
+        for (let j = 0; j < state.n; j++) {
+            totEmpty += state.board[j][i] === '' ? inc : 0;
+        }
+        if (Math.abs(totEmpty) < movesLeft && Math.abs(state.colSum[i] + totEmpty) === state.n) {
+            //console.log(`Col ${i} Empty in Col: ${totEmpty} Col Sum: ${state.colSum[i]}`);
+            return true;
+        }
+    }
+
+    let totEmpty = 0;
+    let i = 0;
+    let j = 0;
+    while (i < state.n && j < state.n) {
+        totEmpty += state.board[i][j] === '' ? inc : 0;
+        i++;
+        j++;
+    }
+    if (Math.abs(totEmpty) < movesLeft && Math.abs(state.mainDiagSum + totEmpty) === state.n) {
+        //console.log(`Empty in Main Diag: ${totEmpty} Main Diag Sum: ${state.mainDiagSum}`);
+        return true;
+    }
+
+    totEmpty = 0;
+    i = 0;
+    j = state.n - 1;
+    while (i < state.n && j >= 0) {
+        totEmpty += state.board[i][j] === '' ? inc : 0;
+        i++;
+        j--;
+    }
+    //console.log(`Empty in Off Diag: ${totEmpty} Off Diag Sum: ${state.offDiagSum}`);
+    return Math.abs(totEmpty) < movesLeft && Math.abs(state.offDiagSum + totEmpty) === state.n;
+}
+
+function emptyBoardState(n: number) {
+    return {
+        n: n,
+        rowSum: Array(n).fill(0),
+        colSum: Array(n).fill(0),
+        mainDiagSum: 0,
+        offDiagSum: 0,
+        gameState: GameState.on,
+        piece: 'X',
+        board: Array(n).fill(Array(n).fill(''))
+    } as ITikTakToeBoardState;
+}
+
+function cloneBoardState(boardState: ITikTakToeBoardState) {
+    const tmp: any = [];
+    boardState.board.forEach(row => tmp.push(Object.assign([], row)));
+    return {
+        n: boardState.n,
+        rowSum: Object.assign([], boardState.rowSum),
+        colSum: Object.assign([], boardState.colSum),
+        mainDiagSum: boardState.mainDiagSum,
+        offDiagSum: boardState.offDiagSum,
+        gameState: boardState.gameState,
+        piece: boardState.piece,
+        board: tmp
+    } as ITikTakToeBoardState;
+}
+
 const TikTakToe = (props: React.PropsWithChildren<ITikTakToeProps>) => {
     const { n } = props;
-    const [board, setBoard] = useState(Array(n).fill(Array(n).fill('')));
-    const [piece, setPiece] = useState('X');
-    const [boardState, setBoardState] = useState<ITikTakToeBoardState>(emptyBoardState());
+    const [boardState, setBoardState] = useState<ITikTakToeBoardState>(emptyBoardState(n));
     const [gameState, setGameState] = useState('Game On!');
 
     function updateGameState(gameState: GameState) {
@@ -46,102 +137,24 @@ const TikTakToe = (props: React.PropsWithChildren<ITikTakToeProps>) => {
         }
     }
 
-    function emptyBoardState() {
-        return {
-            n: n,
-            rowSum: Array(n).fill(0),
-            colSum: Array(n).fill(0),
-            mainDiagSum: 0,
-            offDiagSum: 0,
-            gameState: GameState.noWins,
-            winningPiece: ''
-        } as ITikTakToeBoardState;
-    }
+    function updateBoardState(i: number, j: number) {
+        const tmpBoard: any = [];
+        boardState.board.forEach(row => tmpBoard.push(Object.assign([], row)));
+        tmpBoard[i][j] = boardState.piece;
 
-    function cloneBoardState() {
-        return {
-            n: boardState.n,
-            rowSum: Object.assign([], boardState.rowSum),
-            colSum: Object.assign([], boardState.colSum),
-            mainDiagSum: boardState.mainDiagSum,
-            offDiagSum: boardState.offDiagSum,
-            gameState: boardState.gameState,
-            winningPiece: boardState.winningPiece
-        } as ITikTakToeBoardState;
-    }
-
-    function winPossible(tmpState: ITikTakToeBoardState, tmpBoard: any, piece: string): boolean {
-        let remainingEmpty = 0;
-        for (let i = 0; i < n; i++)
-        {
-            for (let j = 0; j < n; j++) {
-                if (tmpBoard[i][j] === '') {
-                    remainingEmpty++;
-                }
-            }
-        }
-
-        /* get numMovesLeft for player to figure out if it's possible to win */
-        const numMovesLeft = Math.floor(remainingEmpty/ 2);
-        const inc = piece === 'X' ? 1 : -1;
-
-        /* find if a row win is possible */
-        for (let i = 0; i < n; i++) {
-            let totEmpty = 0;
-            for (let j = 0; j < n; j++) {
-                totEmpty += tmpBoard[i][j] === '' ? inc : 0;
-            }
-            if (totEmpty < numMovesLeft && Math.abs(tmpState.rowSum[i] + totEmpty) === n) {
-                return true;
-            }
-        }
-
-        /* find if a col win is possible */
-        for (let i = 0; i < n; i++) {
-            let totEmpty = 0;
-            for (let j = 0; j < n; j++) {
-                totEmpty += tmpBoard[j][i] === '' ? inc : 0;
-            }
-            if (totEmpty < numMovesLeft && Math.abs(tmpState.colSum[i] + totEmpty) === n) {
-                return true;
-            }
-        }
-
-        let totEmpty = 0;
-        let i = 0;
-        let j = 0;
-        while (i < n && j < n) {
-            totEmpty += tmpBoard[i][j] === '' ? inc : 0;
-            i++;
-            j++;
-        }
-        if (totEmpty < numMovesLeft && Math.abs(tmpState.mainDiagSum + totEmpty) === n) {
-            return true;
-        }
-        totEmpty = 0;
-        i = 0;
-        j = n - 1;
-        while (i < n && j >= 0) {
-            totEmpty += tmpBoard[i][j] === '' ? inc : 0;
-            i++;
-            j--;
-        }
-        return totEmpty < numMovesLeft && Math.abs(tmpState.offDiagSum + totEmpty) === n;
-    }
-
-    function updateBoardState(i: number, j: number, piece: string, tmpBoard: any) {
-        const tmp = cloneBoardState();
-        tmp.rowSum[i] += piece === 'X' ? 1 : -1;
-        tmp.colSum[j] += piece === 'X' ? 1 : -1;
+        const tmpState = cloneBoardState(boardState);
+        tmpState.board = tmpBoard;
+        const inc = boardState.piece === 'X' ? 1 : -1;
+        tmpState.rowSum[i] += inc;
+        tmpState.colSum[j] += inc;
         if (i === j) {
-            tmp.mainDiagSum += piece === 'X' ? 1 : -1;
+            tmpState.mainDiagSum += inc;
         }
-        if (Math.abs(tmp.n - 1 - j) === i) {
-            tmp.offDiagSum += piece === 'X' ? 1 : -1;
+        if (Math.abs(tmpState.n - 1 - j) === i) {
+            tmpState.offDiagSum += inc;
         }
-        if (Math.abs(tmp.rowSum[i]) === tmp.n || Math.abs(tmp.colSum[j]) === tmp.n || Math.abs(tmp.mainDiagSum) === tmp.n || Math.abs(tmp.offDiagSum) === tmp.n) {
-            tmp.gameState = piece === 'X' ? GameState.xWins : GameState.oWins;
-            tmp.winningPiece = piece;
+        if (Math.abs(tmpState.rowSum[i]) === tmpState.n || Math.abs(tmpState.colSum[j]) === tmpState.n || Math.abs(tmpState.mainDiagSum) === tmpState.n || Math.abs(tmpState.offDiagSum) === tmpState.n) {
+            tmpState.gameState = tmpState.piece === 'X' ? GameState.xWins : GameState.oWins;
         }
 
         var tot = 0;
@@ -150,34 +163,28 @@ const TikTakToe = (props: React.PropsWithChildren<ITikTakToeProps>) => {
                 tot += tmpBoard[i][j] !== '' ? 1 : 0;
             }
         }
-        if (tot === 0 || (tmp.gameState === GameState.noWins && !winPossible(tmp, tmpBoard, piece) && !winPossible(tmp, tmpBoard, piece === 'X' ? 'O' : 'X'))) {
-            tmp.gameState = GameState.tie;
-        }
 
-        console.log(piece, winPossible(tmp, tmpBoard, piece));
-        console.log(piece === 'X' ? 'O' : 'X', winPossible(tmp, tmpBoard, piece === 'X' ? 'O' : 'X'));
-        setBoardState(tmp);
-        updateGameState(tmp.gameState);
+        if (tot === 0 || (tmpState.gameState === GameState.on && !winPossible(tmpState, tmpState.piece === 'X' ? 1 : -1, numMovesLeft(tmpBoard, n, 0)) && !winPossible(tmpState, tmpState.piece === 'X' ? -1 : 1, numMovesLeft(tmpBoard, n, 1)))) {
+            tmpState.gameState = GameState.tie;
+        }
+        tmpState.piece = tmpState.piece === 'X' ? 'O' : 'X';
+        setBoardState(tmpState);
+        updateGameState(tmpState.gameState);
     }
 
     function handleClick(i: number, j: number) {
-        if (board[i][j] !== '' || boardState.gameState !== GameState.noWins) {
+        if (boardState.board[i][j] !== '' || boardState.gameState !== GameState.on) {
             return;
         }
 
-        const tmp: any = [];
-        board.forEach(row => tmp.push(Object.assign([], row)));
-        tmp[i][j] = piece;
-        updateBoardState(i, j, piece, tmp);
-        setBoard(tmp);
-        setPiece(piece === 'X' ? 'O' : 'X');
+        updateBoardState(i, j);
     }
 
     const content = [];
     for (let i = 0; i < n; i++) {
         const tmp = [];
         for (let j = 0; j < n; j++) {
-            tmp.push(<button className="box" key={i.toString() + "|" + j.toString()} onClick={() => handleClick(i, j)}>{board[i][j]}</button>);
+            tmp.push(<button className="box" key={i.toString() + "|" + j.toString()} onClick={() => handleClick(i, j)}>{boardState.board[i][j]}</button>);
         }
         content.push(tmp);
     }
@@ -191,7 +198,11 @@ const TikTakToe = (props: React.PropsWithChildren<ITikTakToeProps>) => {
                 variant="contained"
                 color="secondary"
                 startIcon={<ClearAllIcon />}
-                onClick={() => { setPiece('X'); setBoard(Array(n).fill(Array(n).fill(''))); setBoardState(emptyBoardState()); updateGameState(GameState.noWins)}}
+                onClick={() =>
+                {
+                    setBoardState(emptyBoardState(n));
+                    updateGameState(GameState.on)
+                }}
             >
                 Clear
             </Button>

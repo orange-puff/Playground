5259dbcaa657d072cf051a6637d4a0f83937011e
John Mancini <johnmancini30@gmail.com>
2021-07-25 19:30:23 -0400

    deleted comment

--- src/Playground.Web/UI/src/components/projects/typing_project/TypingProject.tsx
+++ src/Playground.Web/UI/src/components/projects/typing_project/TypingProject.tsx
@@ -146,15 +146,3 @@ const TypingProject = () => {
 }
 
 export default TypingProject;
-
-/*
- * A complete copy of https://www.livechat.com/typing-speed-test/#/
- * Outer div is the entire box
- * Left div which is left half
- * Right div which is right half
- * Middle div which is tiny and just for the caret
- *
- * The right div contains a series of spans, each span contains a word
- * As the user types, correct letters get erased from head of right div and added to tail of left div
- *
- * */

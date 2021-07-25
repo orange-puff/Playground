2f3f562b193ff4c40b95e53b6a1eaf40acce992b
John Mancini <johnmancini30@gmail.com>
2021-07-24 22:36:26 -0400

    added useful comment

--- src/Playground.Web/UI/src/components/projects/typing_project/TypingProject.tsx
+++ src/Playground.Web/UI/src/components/projects/typing_project/TypingProject.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+}));
 
 const TypingProject = () => {
     return (
@@ -9,3 +13,15 @@ const TypingProject = () => {
 }
 
 export default TypingProject;
+
+/*
+ * A complete copy of https://www.livechat.com/typing-speed-test/#/
+ * Outer div is the entire box
+ * Left div which is left half
+ * Right div which is right half
+ * Middle div which is tiny and just for the caret
+ *
+ * The right div contains a series of spans, each span contains a word
+ * As the user types, correct letters get erased from head of right div and added to tail of left div
+ * 
+ * */
